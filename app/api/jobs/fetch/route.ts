bab15d89e038b163b8af61e4717fa55393a6a4c7
Amir-zeb <amir-zeb@iinv.tech>
2026-02-22 00:07:37 +0500

    create api to fetch jobs

---
+++ app/api/jobs/fetch/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+import { connectDB } from "@/lib/db";
+import Job from "@/lib/models/Job";
+
+export async function GET() {
+    try {
+        await connectDB();
+
+        const res = await fetch("https://remoteok.com/api");
+        const data = await res.json();
+
+        // First element is metadata, skip it
+        const jobs = data.slice(1);
+
+        for (const job of jobs) {
+            await Job.updateOne(
+                { url: job.url },
+                {
+                    title: job.position,
+                    company: job.company,
+                    location: job.location,
+                    salary: job.salary,
+                    salary_min: job.salary_min,
+                    salary_max: job.salary_max,
+                    url: job.url,
+                    post_date: new Date(job.date),
+                    description: job.description,
+                    source: "RemoteOK",
+                },
+                { upsert: true }
+            );
+        }
+        return NextResponse.json({ message: "Jobs fetched successfully", data: jobs });
+    } catch (error) {
+        return NextResponse.json({ error, message: "Something went wrong" }, { status: 500 });
+    }
+
+}
