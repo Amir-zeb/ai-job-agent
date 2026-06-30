ed6a656d761d9592d5760250f89d4c8bfea0bf13
Amir-zeb <amirzeb695@gmail.com>
2026-06-30 17:21:02 +0500

    jobCard - update colors and styling

--- app/jobs/_components/jobCard.tsx
+++ app/jobs/_components/jobCard.tsx
@@ -1,50 +1,39 @@
 'use client'
 
+import { Badge } from "@/app/components/badge";
 import { JobT } from "@/lib/types";
 
 type Props = {
     job: JobT;
-    setSelectedJob: (job: JobT) => void;
+    selectedJobId: string | null;
+    setSelectedJob: () => void;
 };
 
-const JobCard = ({ job, setSelectedJob }: Props) => {
+const JobCard = ({ job, selectedJobId, setSelectedJob }: Props) => {
     const score = Number(job?.analysis?.aiScore || 0);
     const isAnalyzed = job?.analysis?.isAnalyzed || false
     const aiReason = job?.analysis?.reason || 'Job not analyzed yet.'
 
-    const getBadgeStyles = () => {
-        if (!isAnalyzed) {
-            return "bg-gray-100 text-gray-500 border-gray-200";
-        }
-
-        if (score >= 80) {
-            return "bg-green-100 text-green-700 border-green-200";
-        }
-
-        if (score >= 50) {
-            return "bg-blue-100 text-blue-700 border-blue-200";
-        }
-
-        return "bg-red-100 text-red-600 border-red-200";
-    };
-
     return (
         <button
-            onClick={() => setSelectedJob(job)}
+            onClick={() => setSelectedJob()}
+            disabled={selectedJobId === job?._id}
             className="w-full text-left transition-transform duration-200 hover:-translate-y-1"
         >
-            <div className="bg-white border rounded-xl p-5 shadow-sm hover:shadow-md transition-shadow duration-200">
-
+            <div className="bg-(--primary) relative border rounded-xl p-5 shadow-sm hover:shadow-md transition-shadow duration-200">
+                {selectedJobId === job?._id &&
+                    <div className="absolute top-2.5 left-2.5 w-2.5 h-2.5 bg-(--secondary) rounded-full" />
+                }
                 {/* Header */}
                 <div className="flex justify-between items-start gap-3">
                     <div>
-                        <h3 className="text-lg font-semibold text-gray-900 line-clamp-2">
+                        <h3 className="text-lg font-semibold text-(--secondary) line-clamp-2">
                             {job.title || "-"}
                         </h3>
-                        <p className="text-sm text-gray-600 mt-1">
+                        <p className="text-sm text-(--secondary) mt-1">
                             {job.company || "-"}
                         </p>
-                        <p className="text-xs text-gray-400">
+                        <p className="text-xs text-(--secondary)">
                             {job.location || "Remote"}
                         </p>
                     </div>
@@ -53,29 +42,21 @@ const JobCard = ({ job, setSelectedJob }: Props) => {
                         {/* Score Badge */}
                         {isAnalyzed ?
                             <>
-                                <span
-                                    className={`px-3 py-1 text-xs font-semibold rounded-full border ${getBadgeStyles()}`}
-                                >
-                                    {isAnalyzed ? `Ai Score: ${score}` : "Not Rated"}
-                                </span>
-                                <span
-                                    className={`px-3 py-1 text-xs font-semibold rounded-full border ${getBadgeStyles()}`}
-                                >
-                                    {isAnalyzed ? `Recommendation: ${job?.analysis?.recommendation}` : ""}
-                                </span>
+                                <Badge
+                                    label={isAnalyzed ? `Ai Score: ${score}` : "Not Rated"}
+                                />
+                                <Badge
+                                    label={isAnalyzed ? `Recommendation: ${job?.analysis?.recommendation}` : ""}
+                                />
                             </> :
                             <>
                                 {/* Rule-Based Score Badge */}
-                                <span
-                                    className={`px-3 py-1 text-xs font-semibold rounded-full border ${getBadgeStyles()}`}
-                                >
-                                    {job.ruleBasedScore !== undefined ? `Score: ${job.ruleBasedScore}` : "Not Rated"}
-                                </span>
-                                <span
-                                    className={`px-3 py-1 text-xs font-semibold rounded-full border ${getBadgeStyles()}`}
-                                >
-                                    {job.isRelevant !== undefined ? `Relevance: ${job.isRelevant ? 'High' : 'Low'}` : "Not Rated"}
-                                </span>
+                                <Badge
+                                    label={job.ruleBasedScore !== undefined ? `Score: ${job.ruleBasedScore}` : "Not Rated"}
+                                />
+                                <Badge
+                                    label={job.isRelevant !== undefined ? `Relevance: ${job.isRelevant ? 'High' : 'Low'}` : "Not Rated"}
+                                />
                             </>
                         }
                     </div>
@@ -85,11 +66,11 @@ const JobCard = ({ job, setSelectedJob }: Props) => {
                 <div className="my-4 border-t" />
 
                 {/* AI Reason */}
-                <div>
-                    <p className="text-xs font-medium text-gray-500 mb-1">
+                <div className="text-(--secondary)">
+                    <p className="text-xs font-medium mb-1">
                         AI Insight
                     </p>
-                    <p className="text-sm text-gray-700 line-clamp-3">
+                    <p className="text-sm line-clamp-3">
                         {aiReason}
                     </p>
                 </div>
